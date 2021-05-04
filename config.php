8d928739f554eec9e0c0a8f19dfe4ccc11032c7c
Enno Hermann <enno.hermann@gmail.com>
2021-05-04 17:53:29 +0200

    Add French translation
    Adapted from:
    https://github.com/EqualStreetNames/equalstreetnames-brussels/blob/d67ca00f2a0e0eec69309c4496345c1c9a5955bf/html/fr/index.html

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ return [
     'relationId' => 1685018,
 
     // Languages in which you want to extract Wiki informations
-    'languages' => ['en'],
+    'languages' => ['en', 'fr'],
 
     // Exclude datas from beeing extracted. Usefull to exclude streets at the edge of the city for example
     'exclude' => [
